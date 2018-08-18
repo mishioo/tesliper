2b362117678b6a590b1ff0f11f1c92536dfda5b4
Michał M. Więcław <wieclawmm@gmail.com>
2018-08-18 21:48:56 +0200

    Project restructured.

--- tesliper/__init__.py
+++ tesliper/__init__.py
@@ -1,3 +1,1 @@
-from .tesliper.tesliper import Tesliper
-from .tesliper.datawork import gaussian, lorentzian, Energies, Bars, Spectra
-from .tesliper.extraction import Soxhlet
+from tesliper.extraction import Soxhlet
