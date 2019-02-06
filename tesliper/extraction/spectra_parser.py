fe383d495e5c22a52a73f9848ad6f852cc2d7b3a
Michał M. Więcław <wieclawmm@gmail.com>
2019-02-07 00:15:23 +0100

    Fixed problem with loading spectra from some txt files.
    extraction.spectra_parser:
        - added module-level logger
    extraction.spectra_parser.SpectraParser:
    	- added .parse_txt(file, delimiter=None, xcolumn=0, ycolumn=1), designed to
          parse text files with unknown number of heder lines, after which comes
          a block of spectral data as floats delimited by 'delimiter' (which
          defaults to whitespace); column 'xcolumn' of this block is interpreted as
          points on x axis and column 'ycolumn' is interpreted as values on y axis;
          returns numpy.array([[x-values], [y-values]], dype=float)
        - modified .parse() to use above mentioned method for parsing files;
          it should be later modified to distinguish between .txt, .csv, and .spc
          files and parse them appropriately

--- tesliper/extraction/spectra_parser.py
+++ tesliper/extraction/spectra_parser.py
@@ -1,4 +1,8 @@
 import numpy as np
+import logging as lgg
+
+
+logger = lgg.getLogger(__name__)
 
 
 class SpectraParser:
@@ -6,22 +10,90 @@ class SpectraParser:
     def __init__(self):
         self.max_lines_omitted = 20
 
-    def parse(self, filename, mode='txt', delimiter=None):
-        arr = None
-        if mode == 'txt':
-            try:
-                arr = np.loadtxt(
-                    filename, delimiter=delimiter, usecols=(0, 1), unpack=True
+    def parse(self, filename, delimiter=None, xcolumn=0, ycolumn=1):
+        """Loads spectral data from file to numpy.array. Currently supports
+        only .txt files.
+
+        Parameters
+        ----------
+        filename: str
+            path to file containing spectral data
+        delimiter: str, optional
+            character used to delimit columns in file, defaults to whitespace
+        xcolumn: int, optional
+            column, that should be used as points on x axis,
+            defaults to 0 (first column)
+        ycolumn: int, optional
+            column, that should be used as values on y axis,
+            defaults to 1 (second column)
+
+        Returns
+        -------
+        numpy.array
+            two-dimensional numpy array ([[x-values], [y-values]])
+            of data type 'float'
+
+        TO DO
+        -----
+        add type checking of passed file, consider those:
+            https://github.com/audreyr/binaryornot
+            https://eli.thegreenplace.net/2011/10/19/perls-guess-if-file-is-text-or-binary-implemented-in-python/
+        add csv and binary files support"""
+        return self.parse_txt(filename, delimiter, xcolumn, ycolumn)
+
+    def parse_txt(self, file, delimiter=None, xcolumn=0, ycolumn=1):
+        """Loads spectral data from txt file to numpy.array.
+
+        Parameters
+        ----------
+        file: str
+            path to file containing spectral data
+        delimiter: str, optional
+            character used to delimit columns in file, defaults to whitespace
+        xcolumn: int, optional
+            column, that should be used as points on x axis,
+            defaults to 0 (first column)
+        ycolumn: int, optional
+            column, that should be used as values on y axis,
+            defaults to 1 (second column)
+
+        Returns
+        -------
+        numpy.array
+            two-dimensional numpy array ([[x-values], [y-values]])
+            of data type 'float'
+
+        Rises
+        -----
+        ValueError
+            if file passed was read to end, but no spectral data wa found;
+            this includes columns' numbers out of range and usage of
+            inappropriate delimiter"""
+        with open(file, 'r') as txtfile:
+            line = txtfile.readline()
+            lineno = 1
+            search = True
+            while line and search:
+                try:
+                    values = [
+                        float(v.strip()) for v in line.split(delimiter) if v
+                    ]
+                    x, y = values[xcolumn], values[ycolumn]
+                    search = False
+                except (ValueError, TypeError, IndexError) as error:
+                    logger.debug(f"Line omitted due to {error}")
+                    line = txtfile.readline()
+                    lineno += 1
+            if not line:
+                raise ValueError(
+                    f"No spectral data found in file's columns {xcolumn} "
+                    f"and {ycolumn}."
                 )
-            except ValueError:
-                for n in range(self.max_lines_omitted):
-                    try:
-                        arr = np.loadtxt(filename, delimiter=delimiter,
-                                         usecols=(0, 1), unpack=True,
-                                         skiprows=n)
-                    except ValueError:
-                        pass
-                    finally:
-                        if arr is None:
-                            raise
-            return arr
+            arr = [(x, y)]
+            for lineno, line in enumerate(txtfile, start=lineno+1):
+                values = [
+                    float(v.strip()) for v in line.split(delimiter) if v
+                ]
+                arr.append((values[xcolumn], values[ycolumn]))
+        return np.array(list(zip(*arr)))
+
