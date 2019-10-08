7379fc7d72fd58118bd1b6c9897b3964700ea72f
Michał M. Więcław <wieclawmm@gmail.com>
2019-10-08 23:13:03 +0200

    Tweaked tests imports. Started work on Soxhlet tests.

--- test/unit/__init__.py
+++ test/unit/__init__.py
@@ -1,6 +1,5 @@
 from .extraction import *
 from .datawork import *
 from .glassware import *
-from .glassware_test import *
 from .tesliper_test import *
 from .writer_test import *
--- test/unit/glassware/__init__.py
+++ test/unit/glassware/__init__.py
@@ -1,3 +1,4 @@
 from .array_base_test import *
 from .arrays_test import *
 from .nmr_test import *
+from .molecules_test import *
