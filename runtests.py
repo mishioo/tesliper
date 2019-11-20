d654e4632e90bbdeb98e9d24025fb6523a956341
Michał M. Więcław <wieclawmm@gmail.com>
2019-11-20 22:35:02 +0100

    Integrated with Travis-CI.

--- runtests.py
+++
@@ -1,9 +0,0 @@
-from test.unit.extraction.test_gaussian_parser import *
-from test.unit.extraction.test_extraction import *
-from test.unit.glassware_test import *
-from test.unit.datawork_test import *
-from test.unit.test_writer import *
-from test.unit.test_tesliper import *
-
-if __name__ == '__main__':
-    unittest.main()
