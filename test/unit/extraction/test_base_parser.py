f973bc814fb0d5eeff4409b5854ed06405ab513c
Michał M. Więcław <wieclawmm@gmail.com>
2020-04-03 18:48:33 +0200

    Ported to pytest: test/unit/extraction/test_base_parser.py

--- test/unit/extraction/test_base_parser.py
+++ test/unit/extraction/test_base_parser.py
@@ -1,212 +1,232 @@
-from unittest import TestCase
 from unittest.mock import Mock, NonCallableMock, patch
 import re
 from tesliper.extraction import Parser
 from tesliper.exceptions import InvalidStateError
+import pytest
 
 
-class TestParser(TestCase):
-    def setUp(self):
-        cls = type(
-            "Prsr", (Parser,), {"initial": Mock(spec=[]), "parse": Mock(spec=[])}
-        )
-        self.prs = cls()
-        self.method = Mock(spec=[])
+@pytest.fixture
+def prs():
+    cls = type("Prsr", (Parser,), {"initial": Mock(spec=[]), "parse": Mock(spec=[])})
+    return cls()
 
-    def test_instantiated_directly(self):
-        self.assertRaises(TypeError, Parser)
 
-    def test_state_no_arguments(self):
-        self.assertRaises(TypeError, Parser.state)
+@pytest.fixture
+def method():
+    return Mock(spec=[])
 
-    def test_state_not_callable(self):
-        func = NonCallableMock()
-        self.assertRaises(InvalidStateError, Parser.state, func)
 
-    def test_state_decorator(self):
-        func = Parser.state(self.method)
-        self.assertTrue(hasattr(func, "is_state"))
-        self.assertTrue(func.is_state)
+def test_instantiated_directly():
+    with pytest.raises(TypeError):
+        Parser()
 
-    def test_state_decorator_with_trigger(self):
-        func = Parser.state(self.method, "mytrigger")
-        self.assertTrue(hasattr(func, "is_state"))
-        self.assertTrue(func.is_state)
-        self.assertTrue(hasattr(func, "trigger"))
-        self.assertTrue(func.trigger == "mytrigger")
 
-    def test_subclassed(self):
-        self.assertIn("initial", self.prs.states)
-        self.assertEqual(self.prs.initial, self.prs.workhorse)
+def test_state_no_arguments():
+    with pytest.raises(TypeError):
+        Parser.state()
 
-    def test_subclassed_with_state(self):
-        func = self.method
-        func.is_state = True
-        cls = type(
-            "Prsr",
-            (Parser,),
-            {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
-        )
-        obj = cls()
-        self.assertTrue(hasattr(obj, "mystate"))
-        self.assertIn("mystate", obj.states)
-        self.assertEqual(obj.initial, obj.workhorse)
 
-    def test_subclassed_with_trigger(self):
-        class Subclass(Parser):
-            def parse(self, lines):
-                pass
+def test_state_not_callable():
+    func = NonCallableMock()
+    with pytest.raises(InvalidStateError):
+        Parser.state(func)
 
-            def initial(self, line):
-                pass
 
-            @Parser.state(trigger="trigger")
-            def mystate(self):
-                pass
+def test_state_decorator(method):
+    func = Parser.state(method)
+    assert hasattr(func, "is_state")
+    assert func.is_state
 
-        with patch("re.compile") as mocked_re_compile:
-            mocked_re_compile.return_value = "compiled_trigger"
-            obj = Subclass()
-        self.assertTrue(hasattr(obj, "mystate"))
-        self.assertTrue(hasattr(obj.mystate, "is_state"))
-        self.assertIn("mystate", obj.triggers)
-        self.assertTrue(mocked_re_compile.called)
-        self.assertEqual(obj.triggers["mystate"], "compiled_trigger")
 
-    def test_add_state(self):
-        func = self.method
-        func.__name__ = "mystate"
-        self.prs.add_state(func)
-        self.assertIn("mystate", self.prs.states)
+def test_state_decorator_with_trigger(method):
+    func = Parser.state(method, "mytrigger")
+    assert hasattr(func, "is_state")
+    assert func.is_state
+    assert hasattr(func, "trigger")
+    assert func.trigger == "mytrigger"
 
-    def test_remove_state(self):
-        func = self.method
-        func.is_state = True
-        cls = type(
-            "Prsr",
-            (Parser,),
-            {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
-        )
-        obj = cls()
-        self.assertIn("mystate", obj.states)
-        obj.remove_state("mystate")
-        self.assertNotIn("mystate", obj.states)
 
-    def test_add_state_with_trigger_as_string(self):
-        func = self.method
-        func.__name__ = "mystate"
-        func.trigger = "regex"
-        self.prs.add_state(func)
-        self.assertIn("mystate", self.prs.states)
-        self.assertIn("mystate", self.prs.triggers)
-        self.assertEqual(self.prs.triggers["mystate"], re.compile("regex"))
+def test_subclassed(prs):
+    assert "initial" in prs.states
+    assert prs.initial == prs.workhorse
 
-    def test_add_state_with_trigger_as_regex(self):
-        func = self.method
-        func.__name__ = "mystate"
-        func.trigger = re.compile("regex")
-        self.prs.add_state(func)
-        self.assertIn("mystate", self.prs.states)
-        self.assertIn("mystate", self.prs.triggers)
-        self.assertEqual(self.prs.triggers["mystate"], re.compile("regex"))
 
-    def test_remove_state_with_trigger(self):
-        func = self.method
-        func.is_state = True
-        func.trigger = "regex"
-        cls = type(
-            "Prsr",
-            (Parser,),
-            {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
-        )
-        obj = cls()
-        self.assertIn("mystate", obj.states)
-        self.assertIn("mystate", obj.triggers)
-        obj.remove_state("mystate")
-        self.assertNotIn("mystate", obj.states)
-        self.assertNotIn("mystate", obj.triggers)
+def test_subclassed_with_state(method):
+    func = method
+    func.is_state = True
+    cls = type(
+        "Prsr",
+        (Parser,),
+        {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
+    )
+    obj = cls()
+    assert hasattr(obj, "mystate")
+    assert "mystate" in obj.states
+    assert obj.initial == obj.workhorse
 
-    def test_remove_invalid(self):
-        self.assertRaises(InvalidStateError, self.prs.remove_state, "invalid")
 
-    def test_set_workhorse(self):
-        func = self.method
-        self.prs.workhorse = func
-        self.assertIs(self.prs.workhorse, func)
+def test_subclassed_with_trigger():
+    class Subclass(Parser):
+        def parse(self, lines):
+            pass
 
-    def test_set_workhorse_name(self):
-        func = self.method
-        func.is_state = True
-        cls = type(
-            "Prsr",
-            (Parser,),
-            {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
-        )
-        obj = cls()
-        obj.workhorse = "mystate"
-        self.assertIs(obj.workhorse, func)
+        def initial(self, line):
+            pass
 
-    def test_set_workhorse_invalid(self):
-        def func():
-            self.prs.workhorse = "invalid"
+        @Parser.state(trigger="trigger")
+        def mystate(prs, method):
+            pass
 
-        self.assertRaises(InvalidStateError, func)
+    with patch("re.compile") as mocked_re_compile:
+        mocked_re_compile.return_value = "compiled_trigger"
+        obj = Subclass()
+    assert hasattr(obj, "mystate")
+    assert hasattr(obj.mystate, "is_state")
+    assert "mystate" in obj.triggers
+    assert mocked_re_compile.called
+    assert obj.triggers["mystate"] == "compiled_trigger"
 
-    def test_initial_matching(self):
-        self.prs.initial.side_effect = lambda line: super(
-            type(self.prs), self.prs
-        ).initial(line)
-        self.prs.states["mystate"] = self.method
-        self.prs.triggers["mystate"] = re.compile("yes")
-        out = self.prs.initial("yes, this line will match")
-        self.prs.initial.assert_called()
-        self.assertEqual(out, {})
-        self.assertIs(self.prs.workhorse, self.method)
 
-    def test_initial_not_matching(self):
-        self.prs.initial.side_effect = lambda line: super(
-            type(self.prs), self.prs
-        ).initial(line)
-        self.prs.states["mystate"] = self.method
-        self.prs.triggers["mystate"] = re.compile("yes")
-        out = self.prs.initial("this line will not match")
-        self.prs.initial.assert_called()
-        self.assertDictEqual(out, {})
-        self.assertIs(self.prs.workhorse, self.prs.initial)
+def test_add_state(prs, method):
+    func = method
+    func.__name__ = "mystate"
+    prs.add_state(func)
+    assert "mystate" in prs.states
 
-    def test_parse(self):
-        self.prs.initial.return_value = {"example": "data"}
-        self.prs.parse.side_effect = lambda lines: super(
-            type(self.prs), self.prs
-        ).parse(lines)
-        out = self.prs.parse(["some line"])
-        self.prs.initial.assert_called()
-        self.assertDictEqual(out, {"example": "data"})
 
-    def test_parse_initial_resetting(self):
-        self.method.return_value = {}
-        self.prs.workhorse = self.method
-        self.prs.parse.side_effect = lambda lines: super(
-            type(self.prs), self.prs
-        ).parse(lines)
-        self.prs.parse(["some line"])
-        self.method.assert_called()
-        self.assertIs(self.prs.workhorse, self.prs.initial)
+def test_remove_state(method):
+    func = method
+    func.is_state = True
+    cls = type(
+        "Prsr",
+        (Parser,),
+        {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
+    )
+    obj = cls()
+    assert "mystate" in obj.states
+    obj.remove_state("mystate")
+    assert "mystate" not in obj.states
 
-    def test_parse_raises_not_iterable(self):
-        self.method.return_value = 1
-        self.prs.workhorse = self.method
-        self.prs.parse.side_effect = lambda lines: super(
-            type(self.prs), self.prs
-        ).parse(lines)
-        self.assertRaises(InvalidStateError, self.prs.parse, ["some line"])
-        self.assertIs(self.prs.workhorse, self.prs.initial)
 
-    def test_parse_raises_cant_convert_value(self):
-        self.method.return_value = "wrong sequence"
-        self.prs.workhorse = self.method
-        self.prs.parse.side_effect = lambda lines: super(
-            type(self.prs), self.prs
-        ).parse(lines)
-        self.assertRaises(InvalidStateError, self.prs.parse, ["some line"])
-        self.assertIs(self.prs.workhorse, self.prs.initial)
+def test_add_state_with_trigger_as_string(prs, method):
+    func = method
+    func.__name__ = "mystate"
+    func.trigger = "regex"
+    prs.add_state(func)
+    assert "mystate" in prs.states
+    assert "mystate" in prs.triggers
+    assert prs.triggers["mystate"] == re.compile("regex")
+
+
+def test_add_state_with_trigger_as_regex(prs, method):
+    func = method
+    func.__name__ = "mystate"
+    func.trigger = re.compile("regex")
+    prs.add_state(func)
+    assert "mystate" in prs.states
+    assert "mystate" in prs.triggers
+    assert prs.triggers["mystate"] == re.compile("regex")
+
+
+def test_remove_state_with_trigger(method):
+    func = method
+    func.is_state = True
+    func.trigger = "regex"
+    cls = type(
+        "Prsr",
+        (Parser,),
+        {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
+    )
+    obj = cls()
+    assert "mystate" in obj.states
+    assert "mystate" in obj.triggers
+    obj.remove_state("mystate")
+    assert "mystate" not in obj.states
+    assert "mystate" not in obj.triggers
+
+
+def test_remove_invalid(prs):
+    with pytest.raises(InvalidStateError):
+        prs.remove_state("invalid")
+
+
+def test_set_workhorse(prs, method):
+    func = method
+    prs.workhorse = func
+    assert prs.workhorse is func
+
+
+def test_set_workhorse_name(method):
+    func = method
+    func.is_state = True
+    cls = type(
+        "Prsr",
+        (Parser,),
+        {"initial": Mock(spec=[]), "parse": Mock(spec=[]), "mystate": func},
+    )
+    obj = cls()
+    obj.workhorse = "mystate"
+    assert obj.workhorse is func
+
+
+def test_set_workhorse_invalid(prs):
+    def func():
+        prs.workhorse = "invalid"
+
+    with pytest.raises(InvalidStateError):
+        func()
+
+
+def test_initial_matching(prs, method):
+    prs.initial.side_effect = lambda line: super(type(prs), prs).initial(line)
+    prs.states["mystate"] = method
+    prs.triggers["mystate"] = re.compile("yes")
+    out = prs.initial("yes, this line will match")
+    prs.initial.assert_called()
+    assert out == {}
+    assert prs.workhorse is method
+
+
+def test_initial_not_matching(prs, method):
+    prs.initial.side_effect = lambda line: super(type(prs), prs).initial(line)
+    prs.states["mystate"] = method
+    prs.triggers["mystate"] = re.compile("yes")
+    out = prs.initial("this line will not match")
+    prs.initial.assert_called()
+    assert out == {}
+    assert prs.workhorse is prs.initial
+
+
+def test_parse(prs):
+    prs.initial.return_value = {"example": "data"}
+    prs.parse.side_effect = lambda lines: super(type(prs), prs).parse(lines)
+    out = prs.parse(["some line"])
+    prs.initial.assert_called()
+    assert out == {"example": "data"}
+
+
+def test_parse_initial_resetting(prs, method):
+    method.return_value = {}
+    prs.workhorse = method
+    prs.parse.side_effect = lambda lines: super(type(prs), prs).parse(lines)
+    prs.parse(["some line"])
+    method.assert_called()
+    assert prs.workhorse is prs.initial
+
+
+def test_parse_raises_not_iterable(prs, method):
+    method.return_value = 1
+    prs.workhorse = method
+    prs.parse.side_effect = lambda lines: super(type(prs), prs).parse(lines)
+    with pytest.raises(InvalidStateError):
+        prs.parse(["some line"])
+    assert prs.workhorse is prs.initial
+
+
+def test_parse_raises_cant_convert_value(prs, method):
+    method.return_value = "wrong sequence"
+    prs.workhorse = method
+    prs.parse.side_effect = lambda lines: super(type(prs), prs).parse(lines)
+    with pytest.raises(InvalidStateError):
+        prs.parse(["some line"])
+    assert prs.workhorse is prs.initial
