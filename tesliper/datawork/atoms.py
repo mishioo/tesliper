023329827f21ace4cee4c33b381dd698129c9f94
Michał M. Więcław <wieclawmm@gmail.com>
2020-10-06 13:26:19 +0200

    Updated list of chemical elements symbols.

--- tesliper/datawork/atoms.py
+++ tesliper/datawork/atoms.py
@@ -7,7 +7,7 @@ atomicnums = (
     "Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba "
     "La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb "
     "Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt "
-    "Ds Rg Cn Uut Fl Uup Lv Uus Uuo".split()
+    "Ds Rg Cn Nh Fl Mc Lv Ts Og".split()
 )
 atomicnums = {at: num + 1 for num, at in enumerate(atomicnums)}
 atoms_symbols = {v: k for k, v in atomicnums.items()}
