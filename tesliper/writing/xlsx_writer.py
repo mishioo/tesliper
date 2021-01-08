5e4e9f9f865d0312cd23f41d79f18f94ca7a94fc
Michał M. Więcław <wieclawmm@gmail.com>
2021-01-09 00:00:03 +0100

    XlsxWriter changed to use updated Writer's interface.  - Removed `filename` parameter from methods' signatures, all methods save to `self.destination` now.  - Moved workbook creation to `__init__()`, it also handles removing empty default sheet.  - Enabled using existing file. If 'a' mode is used, workbook is loaded from given destination.  - `.spectra()` and `.single_spectrum()` now accepts only one Spectra and SingleSpectrum object like other Writer objects.  - Added type hints.

--- tesliper/writing/xlsx_writer.py
+++ tesliper/writing/xlsx_writer.py
@@ -1,19 +1,31 @@
 # IMPORTS
 import logging as lgg
+from pathlib import Path
+from typing import Sequence, Optional, Union, Iterable
+
 import openpyxl as oxl
 from itertools import zip_longest
 
 from ._writer import Writer
+from ..glassware.arrays import Energies, Bars, InfoArray, FloatArray, DataArray
+from ..glassware.spectra import Spectra, SingleSpectrum
 
 
 # LOGGER
-
 logger = lgg.getLogger(__name__)
 logger.setLevel(lgg.DEBUG)
 
 
 # CLASSES
 class XlsxWriter(Writer):
+    def __init__(self, destination: Union[str, Path], mode: str = "x"):
+        super().__init__(destination=destination, mode=mode)
+        if self.mode == "a":
+            self.workbook = oxl.load_workbook(self.destination)
+        else:
+            self.workbook = oxl.Workbook()
+            self.workbook.remove(self.workbook.active)
+
     def write(self, data):
         data = self.distribute_data(data)
         if data["energies"]:
@@ -45,15 +57,17 @@ class XlsxWriter(Writer):
             pass
 
     def energies(
-        self, filename, energies, frequencies=None, stoichiometry=None, corrections=None
+        self,
+        energies: Sequence[Energies],
+        frequencies: Optional[DataArray] = None,
+        stoichiometry: Optional[InfoArray] = None,
+        corrections: Sequence[FloatArray] = None,
     ):
         """Writes detailed information from multiple Energies objects to
          single xlsx file.
 
         Parameters
         ----------
-        filename: string
-            path to file
         energies: list of glassware.Energies
             Energies objects that is to be exported
         frequencies: glassware.DataArray, optional
@@ -62,10 +76,9 @@ class XlsxWriter(Writer):
             InfoArray object containing stoichiometry information
         corrections: list of glassware.DataArray
             DataArray objects containing energies corrections"""
-        wb = oxl.Workbook()
-        ws = wb.active
+        wb = self.workbook
+        ws = wb.create_sheet(title="Collective overview")
         ens_no = len(energies)
-        ws.title = "Collective overview"
         headers = ["Gaussian output file", "Populations / %", "Energies / hartree"]
         headers += ["Imag"] if frequencies is not None else []
         headers += ["Stoichiometry"] if stoichiometry is not None else []
@@ -88,7 +101,6 @@ class XlsxWriter(Writer):
         if frequencies is not None and stoichiometry is not None:
             ws.merge_cells(f"{chr(67+2*ens_no)}1:{chr(67+2*ens_no)}2")
         ws.freeze_panes = "A3"
-        # data = self.ts.energies
         filenames = energies[0].filenames
         fmts = (
             ["0"]
@@ -154,16 +166,14 @@ class XlsxWriter(Writer):
                 if not width:
                     width = max(len(str(cell.value)) for cell in column) + 2
                 ws.column_dimensions[column[0].column].width = width
-        wb.save(self.destination.joinpath(filename))
+        wb.save(self.destination)
         logger.info("Energies export to xlsx files done.")
 
-    def bars(self, filename, band, bars):
+    def bars(self, band: Bars, bars: Iterable[Bars]):
         """Writes Bars objects to xlsx file (one sheet for each conformer).
 
         Parameters
         ----------
-        filename: string
-            path to file
         band: glassware.Bars
             object containing information about band at which transitions occur;
             it should be frequencies for vibrational data and wavelengths or
@@ -172,16 +182,15 @@ class XlsxWriter(Writer):
             Bars objects that are to be serialized; all should contain
             information for the same conformers"""
         # TODO: sort on sheets by type of DataArray class (GroundState, ExitedState...)
-        wb = oxl.Workbook()
-        wb.remove(wb.active)
-        bars = [band] + bars
+        wb = self.workbook
+        bars = [band] + list(bars)
         genres = [bar.genre for bar in bars]
         headers = [self._header[genre] for genre in genres]
         widths = [max(len(h), 10) for h in headers]
         fmts = [self._excel_formats[genre] for genre in genres]
         values = list(zip(*[bar.values for bar in bars]))
         for fname, values_ in zip(bars[0].filenames, values):
-            ws = wb.create_sheet(fname)
+            ws = wb.create_sheet(title=fname)
             ws.append(headers)
             ws.freeze_panes = "B2"
             for column, width in zip(ws.columns, widths):
@@ -191,39 +200,32 @@ class XlsxWriter(Writer):
                     cell = ws.cell(row=row_num + 2, column=col_num + 1)
                     cell.value = v
                     cell.number_format = fmt
-        wb.save(self.destination.joinpath(filename))
+        wb.save(self.destination)
         logger.info("Bars export to xlsx files done.")
 
-    def spectra(self, filename, spectra):
-        wb = oxl.Workbook()
-        del wb["Sheet"]
-        for spectra_ in spectra:
-            ws = wb.create_sheet()
-            ws.title = spectra_.genre
-            ws.freeze_panes = "B2"
-            A0 = spectra_.units["x"]
-            ws.append([A0] + list(spectra_.filenames))
-            title = (
-                f"{spectra_.genre} calculated with peak width = "
-                f'{spectra_.width} {spectra_.units["width"]} and '
-                f"{spectra_.fitting} fitting, shown as "
-                f'{spectra_.units["x"]} vs. {spectra_.units["y"]}'
-            )
-            ws["A1"].comment = oxl.comments.Comment(title, "Tesliper")
-            for line in zip(spectra_.x, *spectra_.y):
-                ws.append(line)
-        wb.save(self.destination.joinpath(filename))
+    def spectra(self, spectra: Spectra):
+        wb = self.workbook
+        ws = wb.create_sheet(title=spectra.genre)
+        ws.freeze_panes = "B2"
+        A0 = spectra.units["x"]
+        ws.append([A0] + list(spectra.filenames))
+        title = (
+            f"{spectra.genre} calculated with peak width = "
+            f'{spectra.width} {spectra.units["width"]} and '
+            f"{spectra.fitting} fitting, shown as "
+            f'{spectra.units["x"]} vs. {spectra.units["y"]}'
+        )
+        ws["A1"].comment = oxl.comments.Comment(title, "Tesliper")
+        for line in zip(spectra.x, *spectra.y):
+            ws.append(line)
+        wb.save(self.destination)
         logger.info("Spectra export to xlsx file done.")
 
-    def single_spectrum(self, filename, spectra):
+    def single_spectrum(self, spectrum: SingleSpectrum):
         # TODO: add comment as in txt export
-        # TODO: think how to do it
-        wb = oxl.Workbook()
-        del wb["Sheet"]
-        for spc in spectra:
-            ws = wb.create_sheet()
-            ws.title = spc.genre + "_" + spc.averaged_by
-            for row in zip(spc.x, spc.y):
-                ws.append(row)
-            wb.save(self.destination.joinpath(filename))
+        wb = self.workbook
+        ws = wb.create_sheet(title=f"{spectrum.genre}_{spectrum.averaged_by}")
+        for row in zip(spectrum.x, spectrum.y):
+            ws.append(row)
+        wb.save(self.destination)
         logger.info("Spectrum export to xlsx files done.")
