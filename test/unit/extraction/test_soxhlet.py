4db65ca4d91321d621033747a5f8f315a49596a1
Michał M. Więcław <wieclawmm@gmail.com>
2021-06-11 11:14:57 +0200

    Updated tests with las source code changes.

--- test/unit/extraction/test_soxhlet.py
+++ test/unit/extraction/test_soxhlet.py
@@ -6,19 +6,27 @@ import pytest
 
 
 @pytest.fixture
-def lsdir():
-    with mock.patch(
-        "tesliper.extraction.soxhlet.Path.iterdir",
-        return_value=[Path(p) for p in "a.out b.out b.gjf".split(" ")],
-    ):
-        with mock.patch(
-            "tesliper.extraction.soxhlet.Path.is_file", return_value=True,
-        ) as lsdir:
-            yield lsdir
+def files():
+    return [Path(p) for p in "a.out b.out b.gjf".split()]
 
 
 @pytest.fixture
-def sox():
+def out_files(files):
+    return [f for f in files if f.suffix == ".out"]
+
+
+@pytest.fixture
+def lsdir(files):
+    with mock.patch.multiple(
+        "tesliper.extraction.soxhlet.Path",
+        iterdir=mock.Mock(return_value=files),
+        is_file=mock.Mock(return_value=True),
+    ):
+        yield
+
+
+@pytest.fixture
+def sox(lsdir):
     yield sx.Soxhlet()
 
 
@@ -33,13 +41,13 @@ def test_path_non_existing_path(sox, monkeypatch):
     sx.Path.is_dir.assert_called()
 
 
-def test_path_ok(lsdir, sox, monkeypatch):
+def test_path_ok(sox, files, monkeypatch):
     monkeypatch.setattr(sx.Path, "is_dir", mock.Mock(return_value=True))
-    sox.path = "\\path\\is\\ok"
-    assert Path("\\path\\is\\ok").resolve() == sox.path
+    path = "\\path\\is\\ok"
+    sox.path = path
+    assert Path(path).resolve() == sox.path
     sx.Path.is_dir.assert_called()
-    lsdir.assert_called_with("\\path\\is\\ok")
-    assert "a.out b.out b.gjf".split(" ") == sox.all_files
+    assert files == sox.all_files
 
 
 def test_filter_files_no_extension(sox):
@@ -47,63 +55,65 @@ def test_filter_files_no_extension(sox):
         sox.filter_files()
 
 
-@pytest.mark.usefixtures("lsdir")
-def test_filter_files(sox):
-    assert "a.out b.out".split(" ") == sox.filter_files(".out")
-    assert ["b.gjf"] == sox.filter_files(".gjf")
+def test_filter_files(sox, out_files):
+    assert out_files == sox.filter_files(".out")
+    assert [Path("b.gjf")] == sox.filter_files(".gjf")
 
 
 def test_filter_files_empty(sox):
     assert [] == sox.filter_files(".log")
 
 
-@pytest.mark.usefixtures("lsdir")
-def test_guess_extension(sox):
+@pytest.fixture
+def log_all_files():
+    return [Path(p) for p in "a.log b.log b.gjf".split()]
+
+
+def test_guess_extension(sox, log_all_files, monkeypatch):
     assert ".out" == sox.guess_extension()
-    sox.files = "a.log b.log b.gjf".split(" ")
-    assert ".log" == sox.guess_extension()
 
 
-def test_guess_extension_mixed(sox):
-    sox.files = "a.out b.log b.gjf".split(" ")
+@pytest.fixture
+def mixed_files():
+    return [Path(p) for p in "a.out b.log b.gjf".split()]
+
+
+def test_guess_extension_mixed(sox, mixed_files, monkeypatch):
+    monkeypatch.setattr(sx.Path, "iterdir", mock.Mock(return_value=mixed_files))
     with pytest.raises(ValueError):
         sox.guess_extension()
 
 
-def test_guess_extension_missing(sox):
-    sox.files = "b.gjf".split(" ")
-    with pytest.raises(TypeError):
+def test_guess_extension_missing(sox, monkeypatch):
+    monkeypatch.setattr(sx.Path, "iterdir", mock.Mock(return_value=[Path("b.gjf")]))
+    with pytest.raises(FileNotFoundError):
         sox.guess_extension()
 
 
-def test_output_files(sox):
-    sox.guess_extension = mock.Mock(return_value=".out")
-    sox.filter_files = mock.Mock(return_value=["a.out", "b.out"])
-    assert ["a.out", "b.out"] == sox.output_files
+def test_output_files(sox, out_files, monkeypatch):
+    monkeypatch.setattr(sx.Soxhlet, "guess_extension", mock.Mock(return_value=".out"))
+    monkeypatch.setattr(sx.Soxhlet, "filter_files", mock.Mock(return_value=out_files))
+    assert out_files == sox.output_files
     sox.guess_extension.assert_called()
     sox.filter_files.assert_called_with(".out")
 
 
-@pytest.mark.usefixtures("lsdir")
-def test_all_files(sox):
-    assert "a.out b.out b.gjf".split(" ") == sox.all_files
+def test_all_files(sox, files):
+    assert files == sox.all_files
 
 
 @pytest.fixture
-def output(monkeypatch):
-    mocked = mock.PropertyMock(return_value=["a.out", "b.out"])
+def output(monkeypatch, out_files):
+    mocked = mock.PropertyMock(return_value=out_files)
     monkeypatch.setattr(sx.Soxhlet, "output_files", mocked)
     return mocked
 
 
-def test_extract_iter(monkeypatch, output):
-    sox = sx.Soxhlet()
-    parser = mock.Mock(parse=mock.Mock(return_value={}))
-    sox.parser = parser
-    with monkeypatch.context() as monkey:
-        monkey.setattr(builtins, "open", mock.mock_open())
-        out = dict(sox.extract_iter())
+def test_extract_iter(sox, monkeypatch, output):
+    monkeypatch.setattr(sx.Path, "open", mock.mock_open())
+    monkeypatch.setattr(sox, "parser", mock.Mock(parse=mock.Mock(return_value={})))
+    out = dict(sox.extract_iter())
     assert {"a.out": {}, "b.out": {}} == out
     output.assert_called()
-    parser.parse.assert_called()
-    assert 2 == parser.parse.call_count
+    sox.parser.parse.assert_called()
+    assert 2 == sox.parser.parse.call_count
