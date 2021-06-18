d23fa8a83f00a9a32e08bac77d832d05dd2ecdc7
Michał M. Więcław <wieclawmm@gmail.com>
2021-06-18 12:35:30 +0200

    ParametersParser conversion of parameters implemented.
    - Moved `._transformers` definition to `.__init__()` for easier integration with ConfigParser's getters/converters. Removed associated property to keep it a private helper.
    - Created `quantity()` and `fitting()` converter functions.
    - Created `.parameters` property, responsible for conversion of parsed values from str to appropriate type.
    - Supplemented docstrings and typehints.
    - Added logger.

--- tesliper/extraction/parameters_parser.py
+++ tesliper/extraction/parameters_parser.py
@@ -1,5 +1,59 @@
 from configparser import ConfigParser, MissingSectionHeaderError, ParsingError
 from pathlib import Path
+from typing import Callable, Union
+import re
+import logging
+
+from .. import datawork as dw
+
+
+logger = logging.getLogger(__name__)
+
+# from Python re docs
+FLOATCRE = re.compile(r"[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?")
+FITTINGCRE = re.compile(r"lorentzian|gaussian")
+
+
+def quantity(s: str) -> float:
+    """Convert to float first occurrence of float-looking part of string `s`,
+    ignoring anything else. Raise `configparser.ParsingError` if float cannot be found.
+
+    Parameters
+    ----------
+    s : str
+        string containing a float
+
+    Returns
+    -------
+    float
+        extracted float value
+    """
+    match = FLOATCRE.search(s)
+    if match:
+        return float(match.group())
+    else:
+        raise ParsingError("Cannot interpret any part of given string as float.")
+
+
+def fitting(s: str) -> Callable:
+    """Get fitting function mentioned in a given string `s`, ignoring anything else.
+    Raise `configparser.ParsingError` if known function name cannot be found.
+
+    Parameters
+    ----------
+    s : str
+        string containing name of fitting function
+
+    Returns
+    -------
+    callable
+        an identified fitting function
+    """
+    match = FITTINGCRE.search(s.lower())
+    try:
+        return getattr(dw, match.group())
+    except AttributeError:
+        raise ParsingError(f"No such fitting function: {match.group()}.")
 
 
 class ParametersParser(ConfigParser):
@@ -12,15 +66,60 @@ class ParametersParser(ConfigParser):
         "fitting function": "fitting",
     }
 
-    DEFAULTS = {}
+    def __init__(self):
+        super().__init__(converters={"quantity": quantity, "fitting": fitting})
+        self._transformers = {
+            "width": self.getquantity,
+            "start": self.getquantity,
+            "stop": self.getquantity,
+            "step": self.getquantity,
+            "fitting": self.getfitting,
+        }
 
     def optionxform(self, optionstr: str) -> str:
+        """Translates option names to desired form - lowercase and standard wording,
+        as defined in ALIASES.
+        """
         optionstr = optionstr.lower()
         if optionstr in self.ALIASES:
             optionstr = self.ALIASES[optionstr]
         return optionstr
 
-    def parse(self, source):
+    @property
+    def parameters(self) -> dict:
+        """Dictionary of parameters for calculating spectra extracted from parsed file
+        and converted to appropriate type."""
+        if len(self.sections()) > 1:
+            raise ParsingError(
+                "Multiple sections in parameters setup file are not supported."
+            )
+        params = {}
+        try:
+            section = self.sections()[0]
+        except IndexError:
+            return params
+        for option in self.options(section):
+            try:
+                params[option] = self._transformers[option](section, option)
+            except ParsingError:
+                logger.warning(
+                    f"Cannot understand value for {option}, it will be ignored."
+                )
+        return params
+
+    def parse(self, source: Union[str, Path]):
+        """Parse given source file to get stored parameters.
+
+        Parameters
+        ----------
+        source : str or Path
+            Path to file with calculations' parameters.
+
+        Returns
+        -------
+        dict
+            Parsed parameters.
+        """
         source = Path(source)
         with source.open() as handle:
             text = handle.read()
@@ -28,18 +127,4 @@ class ParametersParser(ConfigParser):
             self.read_string(text)
         except MissingSectionHeaderError:
             self.read_string("[PARAMETERS]\n")
-        if len(self.sections()) > 1:
-            raise ParsingError(
-                "Multiple sections in parameters setup file are not supported."
-            )
-        params = self[self.sections()[0]]
-        return {key: self.transformers[key](value) for key, value in params.items()}
-
-    _transformers = {}
-
-    @property
-    def transformers(self):
-        # TODO: modify converters and use it to implement functionality for converting
-        #       to floats when not only number in value and to get lorentzian
-        #       and gaussian functions instead of strings
-        return self._transformers
+        return self.parameters
