181d4b842306e3814caa7d70f2a848e34356ec3d
Michał M. Więcław <wieclawmm@gmail.com>
2021-10-28 13:32:53 +0200

    Removed a satisfied TODO note.

--- tesliper/tesliper.py
+++ tesliper/tesliper.py
@@ -415,8 +415,6 @@ class Tesliper:
             "a" (append to existing file), "x" (only write if file doesn't exist yet),
             "w" (overwrite file if it already exists). Defaults to "x".
         """
-        # FIXME: only spectra name in output file name,
-        #        consecutive energies are overridden
         wrt = wr.writer(fmt=fmt, destination=self.output_dir, mode=mode)
         data = [s for s in self.averaged.values() if s]
         wrt.write(data)
