71162163562e28c8d409e54021776d5bdca1b932
M. M. Więcław <wieclawmm@gmail.com>
2021-12-17 09:14:03 +0100

    Enabled deleting session and module invocation. Added proper session creation.

--- tesliper/cli.py
+++ tesliper/cli.py
@@ -1,22 +1,50 @@
-import click
-from tesliper import Tesliper, ArchiveWriter
-from pathlib import Path
 from contextlib import contextmanager
+from pathlib import Path
+
+import click
+
+from .tesliper import Tesliper, __version__
+from .writing.serializer import ArchiveWriter
 
 
 @contextmanager
 def get_tesliper(session: Path, mode: str):
-    t = Tesliper() if not session.exists() else Tesliper.load(session)
+    create = not session.exists()
+    tslr = Tesliper() if create else Tesliper.load(session)
     with ArchiveWriter(session, mode) as archive:
-        yield {"tslr": t, "arch": archive}
+        if create:
+            archive.write(tslr)
+        yield {"tslr": tslr, "arch": archive}
 
 
-@click.group()
-@click.option("-s", "--session", type=Path, show_default=True, default=".tslr")
+# TODO: add confirmation?
+def delete_session(ctx, value, param):
+    if not value or ctx.resilient_parsing:
+        return
+    session: Path = ctx.params["session"]
+    if session.exists():
+        session.unlink()
+    ctx.exit()
+
+
+@click.group(chain=True)
+@click.option(
+    "-s", "--session", type=Path, show_default=True, default=".tslr", is_eager=True
+)
 @click.option("-O", "--overwrite/--no-overwrite", default=False)
+@click.option(
+    "-D", "--delete", is_flag=True, callback=delete_session, expose_value=False
+)
+@click.version_option(__version__)
 @click.pass_context
-def tesliper(ctx, session: Path, overwrite: bool):
+def cli(ctx, session: Path, overwrite: bool):
     no_overwrite_mode = "a" if session.exists() else "x"
     ctx.obj = ctx.with_resource(
         get_tesliper(session, mode="w" if overwrite else no_overwrite_mode)
     )
+
+
+@cli.command()
+@click.pass_obj
+def debug(obj):
+    click.echo(obj)
