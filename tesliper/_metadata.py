2977654b970cd153bc34d1443d32b871aab0f0eb
M. M. Więcław <wieclawmm@gmail.com>
2022-02-28 13:23:56 +0100

    Added missing `_metadata:__all__`.

--- tesliper/_metadata.py
+++ tesliper/_metadata.py
@@ -3,3 +3,5 @@ __version_info__ = (0, 9, 1)
 __version__ = ".".join(map(str, __version_info__))
 # approach coined from suggestions in this thread:
 # https://stackoverflow.com/q/458550/11416569
+
+__all__ = ["__author__", "__version_info__", "__version__"]
